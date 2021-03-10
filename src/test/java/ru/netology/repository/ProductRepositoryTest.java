ea3526a4db102089a1abd52bc71345b5c9382ce5
Lis258 <avb-90@ya.ru>
2021-03-10 19:53:02 +0300

    code formatted in ProductRepository.java and ProductRepositoryTest.java

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -9,31 +9,31 @@ import ru.netology.exception.NotFoundException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
-  ProductRepository repository = new ProductRepository();
-  Book book1 = new Book(1, "BookName1", 1000, "Author1", 500, 2019);
-  Book book2 = new Book(2, "BookName2", 500, "Author2", 550, 2017);
-  Book book3 = new Book(3, "BookName3", 700, "Author3", 400, 2020);
-  Book book4 = new Book(4, "BookName4", 400, "Author2", 200, 2016);
+    ProductRepository repository = new ProductRepository();
+    Book book1 = new Book(1, "BookName1", 1000, "Author1", 500, 2019);
+    Book book2 = new Book(2, "BookName2", 500, "Author2", 550, 2017);
+    Book book3 = new Book(3, "BookName3", 700, "Author3", 400, 2020);
+    Book book4 = new Book(4, "BookName4", 400, "Author2", 200, 2016);
 
-  @BeforeEach
-  public void setUp() {
-    repository.save(book1);
-    repository.save(book2);
-    repository.save(book3);
-    repository.save(book4);
-  }
+    @BeforeEach
+    public void setUp() {
+        repository.save(book1);
+        repository.save(book2);
+        repository.save(book3);
+        repository.save(book4);
+    }
 
-  @Test
-  void shouldRemoveByIDIfExistingElement() {
-    repository.removeById(3);
-    Product[] actual = repository.findAll();
-    Product[] expected = new Product[]{book1, book2, book4};
-    assertArrayEquals(expected, actual);
-  }
+    @Test
+    void shouldRemoveByIDIfExistingElement() {
+        repository.removeById(3);
+        Product[] actual = repository.findAll();
+        Product[] expected = new Product[]{book1, book2, book4};
+        assertArrayEquals(expected, actual);
+    }
 
 
-  @Test
-  void shouldRemoveByIDIfNoExistingElement() {
-    assertThrows(NotFoundException.class, () -> repository.removeById(5));
-  }
+    @Test
+    void shouldRemoveByIdNotFoundException() {
+        assertThrows(NotFoundException.class, () -> repository.removeById(5));
+    }
 }
